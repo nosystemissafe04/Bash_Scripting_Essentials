17bb5bc02c274853f55df7ff4a68aa58d6350023
mohammad ali hanfi <99750087+nosystemissafe04@users.noreply.github.com>
2023-07-15 01:13:07 +0530

    Update java_Get_Class.sh

--- java_Get_Class.sh
+++ java_Get_Class.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+#finding out where does all the library files of java are stored 
 java -verbose -version | grep "Opened" > path.txt 2>/dev/null 
-sed -i 's/\[Opened //; s/\]$//' path.txt
-#number_of_lines=$(wc -l < path.txt | cut -f1 -d' ')
-#echo $number_of_lines 
+sed -i 's/\[Opened //; s/\]$//' path.txt 
 mkdir ~/Desktop/JAVA_Classes/
+#collecting all the .class files and storing them on desktop 
 while delimiter= read -r lines;
 do 
      class_name=$(echo "$lines" | sed 's/.*\/\([^\/]*\)$/\1/' | tr -d '\n')
@@ -14,9 +13,9 @@ do
      echo $class_name
      path=~/Desktop/JAVA_Classes/$class_name1/$class_name
      sudo unzip -q $path -d ~/Desktop/JAVA_Classes/$class_name1/ 
-     #sudo jar -xvf ~/Desktop/JAVA_Classes/$class_name1/$class_name
 done < ~/path.txt
 sudo find ~/Desktop/JAVA_Classes/. -type d > change_perm.txt
+#code to change the permission of the files and folder
 while delimiter= read -r lines
 do 
   sudo chown -R $USER:$USER $lines 
